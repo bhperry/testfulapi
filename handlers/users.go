6284fd70e3d9d3a5990e41e4a23502c91a0bd485
Ben <bhperry94@gmail.com>
2017-08-05 17:49:59 -0500

    Moved handlers to separate file, started implementing user update functionality

---
+++ handlers/users.go
@@ -0,0 +1,279 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"database/sql"
+	"encoding/json"
+	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/satori/go.uuid"
+	_ "github.com/go-sql-driver/mysql"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	DB_USER string = "root"
+	DB_PASSWORD string = "password1234"
+	DB_SCHEMA string = "userdb"
+)
+
+var store = sessions.NewCookieStore([]byte("super-duper-ultra-mega-secret-key"))
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email string `json:"email"`
+	Address string `json:"address"`
+	Phone string `json:"phone"`
+	Extra map[string]string `json:"extra"`
+}
+
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "session")
+	if err != nil {
+		HttpResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	authenticated := session.Values["authenticated"]
+	if authenticated != nil {
+		username := fmt.Sprintf("%s", session.Values["username"])
+		details, err := GetUserDetails(username)
+		if err != nil {
+			HttpResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		println(details)
+		json.NewEncoder(w).Encode(details)
+	} else {
+		json.NewEncoder(w).Encode("Hello, world!")
+	}
+}
+
+func NewUserHandler(w http.ResponseWriter, r *http.Request) {
+	db := OpenDB()
+	defer db.Close()
+
+	var user User
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&user)
+
+	newUuid := uuid.NewV4()
+	insert, err := db.Prepare("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		HttpResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer insert.Close()
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	_, err = insert.Exec(newUuid, user.Username, passwordHash, user.Email, user.Address, user.Phone)
+
+	if CheckErr(err) {
+		if strings.Contains(err.Error(), "Error 1062") {
+			HttpResponse(w, http.StatusConflict, "This username is taken")
+		} else {
+			HttpResponse(w, http.StatusInternalServerError, "Error creating new user")
+		}
+	} else {
+		HttpResponse(w, http.StatusOK, "Successfully created new user")
+		//json.NewEncoder(w).Encode("Successfully created new user")
+	}
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	session,_ := store.Get(r, "session")
+	authenticated := session.Values["authenticated"]
+	vars := mux.Vars(r)
+
+	if authenticated != nil {
+		//Check is current user is authorized to access requested data
+		username := fmt.Sprintf("%s", session.Values["username"])
+		if username != vars["username"] {
+			HttpResponse(w, http.StatusUnauthorized, "Unauthorized to access this user's data")
+			return
+		}
+
+		switch r.Method {
+		//Return the user's data
+		case "GET": {
+			details, err := GetUserDetails(username)
+			if err != nil {
+				HttpResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+
+			json.NewEncoder(w).Encode(details)
+			break
+		}
+		//Update the user's data
+		case "PUT": {
+			db := OpenDB()
+			defer db.Close()
+
+			//Decode JSON from PUT request
+			var user User
+			decoder := json.NewDecoder(r.Body)
+			decoder.Decode(&user)
+
+			updateString := ""
+			updateValues := make([]interface{}, 0)
+			if user.Email != "" {
+				updateString += " email = ?"
+				updateValues = append(updateValues, user.Email)
+			}
+			if user.Address != "" {
+				updateString += " address = ?"
+				updateValues = append(updateValues, user.Address)
+			}
+			if user.Phone != "" {
+				updateString += " phone = ?"
+				updateValues = append(updateValues, user.Phone)
+			}
+
+			if updateString != "" {
+				update, err := db.Prepare("UPDATE users SET" + updateString + " WHERE username='" + username + "'")
+				if err != nil {
+					HttpResponse(w, http.StatusInternalServerError, err.Error())
+					return
+				}
+
+				_, err = update.Exec(updateValues...)
+				if err != nil {
+					HttpResponse(w, http.StatusInternalServerError, err.Error())
+					return
+				}
+				HttpResponse(w, http.StatusOK, "Updated user")
+			}
+
+			break
+		}
+		case "DELETE": {
+			db := OpenDB()
+			defer db.Close()
+
+			break
+		}
+		}
+	} else {
+		HttpResponse(w, http.StatusUnauthorized, "Unauthenticated")
+	}
+}
+
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	//Get session data
+	session,_ := store.Get(r, "session")
+
+	switch r.Method {
+	//Authenticate user's login data
+	case "POST": {
+		db := OpenDB()
+		defer db.Close()
+
+		//Decode JSON from POST request
+		var user User
+		decoder := json.NewDecoder(r.Body)
+		decoder.Decode(&user)
+
+		//Check if the username exists and correct password was entered
+		var hashedPassword string
+		err := db.QueryRow("SELECT password FROM users WHERE username = ?", user.Username).Scan(&hashedPassword)
+		if err == sql.ErrNoRows || err != nil {
+			HttpResponse(w, http.StatusUnauthorized, "Invalid username")
+			return
+		}
+
+		//Compare hashed password and user provided password
+		err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password))
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			HttpResponse(w, http.StatusUnauthorized, "Invalid password")
+			return
+		}
+
+		//Create new session token for the user
+		session.Values["username"] = user.Username
+		session.Values["authenticated"] = true
+		session.Save(r, w)
+		HttpResponse(w, http.StatusOK, "Authenticated")
+		break
+	}
+	//Clear the current user's session
+	case "DELETE": {
+		//Set cookie to expire immediately
+		session.Options.MaxAge = -1
+		session.Save(r, w)
+		HttpResponse(w, http.StatusOK, "Session deleted")
+		break
+	}
+	}
+}
+
+func RequestUtilityHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w,r, "static/index.html")
+}
+
+func CheckErr(err error) bool {
+	if err != nil {
+		println(err.Error())
+		return true
+	}
+	return false
+}
+
+func HttpResponse(w http.ResponseWriter, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+	w.Write([]byte(message))
+}
+
+func OpenDB() *sql.DB {
+	//Open database connection
+	var db, err = sql.Open("mysql", DB_USER + ":" + DB_PASSWORD + "@/" + DB_SCHEMA + "?charset=utf8")
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func GetUserDetails(username string) (map[string]string, error) {
+	db := OpenDB()
+	defer db.Close()
+
+	details := make(map[string]string)
+	var userUuid, email, address, phone string
+	queryUser := "SELECT uuid, email, address, phone FROM users WHERE username = ?"
+	err := db.QueryRow(queryUser, username).Scan(&userUuid, &email, &address, &phone)
+	if err == sql.ErrNoRows || err != nil {
+		//HttpResponse(w, http.StatusInternalServerError, err.Error())
+		return nil, err
+	}
+
+	details["username"] = username
+	details["email"] = email
+	details["address"] = address
+	details["phone"] = phone
+
+	rows, err := db.Query("SELECT attr, val FROM user_details WHERE uuid = ?", userUuid)
+
+	//Return only if the error isn't a NoRows error
+	if err != nil && err != sql.ErrNoRows {
+		return nil, err
+	}
+
+	var key, value string
+	for rows.Next() {
+		err = rows.Scan(&key, &value)
+		if err != nil {
+			return nil, err
+		}
+
+		details[key] = value
+	}
+
+	return details, nil
+}
--- server.go
+++ server.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"net/http"
-	"fmt"
-	"database/sql"
 	"flag"
 	"time"
 	"log"
-	"encoding/json"
-	"strings"
 
 	"github.com/urfave/negroni"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
-	"github.com/satori/go.uuid"
-	_ "github.com/go-sql-driver/mysql"
-
-	"golang.org/x/crypto/bcrypt"
-
-	//"github.com/bhperry/testfulapi/handlers"
+	"github.com/bhperry/testfulapi/handlers"
 )
-
-var db, err = sql.Open("mysql", "root:password1234@/userdb?charset=utf8")
-
-var store = sessions.NewCookieStore([]byte("super-duper-ultra-mega-secret-key"))
-
 
 type User struct {
 	Username string `json:"username"`
@@ -38,7 +23,6 @@ type User struct {
 /** Setup routing handlers and start server listening on port 8000
  */
 func main() {
-	defer db.Close()
 	var dir string
 	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
 	flag.Parse()
@@ -50,12 +34,11 @@ func main() {
 
 	//Setup endpoints with their handlers
 	router := mux.NewRouter()
-	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
-	router.HandleFunc("/", IndexHandler).Methods("GET")
-	router.HandleFunc("/user/", NewUserHandler).Methods("POST")
-	router.HandleFunc("/user/{username}/", UserHandler).Methods("GET", "POST", "DELETE")
-	router.HandleFunc("/auth/", AuthHandler).Methods("POST", "DELETE")
-	router.HandleFunc("/utility/", RequestUtilityHandler).Methods("GET")
+	router.HandleFunc("/", handlers.IndexHandler).Methods("GET")
+	router.HandleFunc("/user", handlers.NewUserHandler).Methods("POST")
+	router.HandleFunc("/user/{username}", handlers.UserHandler).Methods("GET", "PUT", "DELETE")
+	router.HandleFunc("/auth", handlers.AuthHandler).Methods("POST", "DELETE")
+	router.HandleFunc("/utility", handlers.RequestUtilityHandler).Methods("GET")
 
 	n := negroni.New()
 	//n.Use(sessions.Sessions("global_session_store", store))
@@ -71,140 +54,8 @@ func main() {
 	//http.ListenAndServe(":8000", n)
 }
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "session")
-	if err != nil {
-		HttpResponse(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	authenticated := session.Values["authenticated"]
-	if authenticated != nil {
-		var userUuid, email, address, phone string
-		username := fmt.Sprintf("%s", session.Values["username"])
-		queryUser := "SELECT uuid, email, address, phone FROM users WHERE username = ?"
-		err := db.QueryRow(queryUser, username).Scan(&userUuid, &email, &address, &phone)
-		if err == sql.ErrNoRows || err != nil {
-			HttpResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		//rows, err := db.Query("SELECT key, val FROM user_details WHERE uuid = ?")
-		fmt.Fprintf(w, "User %s authenticated", session.Values["username"])
-	} else {
-		json.NewEncoder(w).Encode("Hello, world!")
-	}
-}
-
-func NewUserHandler(w http.ResponseWriter, r *http.Request) {
-	var user User
-	decoder := json.NewDecoder(r.Body)
-	decoder.Decode(&user)
-
-	fmt.Printf("USER: %q\n", user)
-
-	newUuid := uuid.NewV4()
-	//TODO: Is this the right format?
-	insert, err := db.Prepare("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?)")
-	CheckErr(err)
-	defer insert.Close()
-
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	_, err = insert.Exec(newUuid, user.Username, passwordHash, user.Email, user.Address, user.Phone)
-
-	if CheckErr(err) {
-		if strings.Contains(err.Error(), "Error 1062") {
-			HttpResponse(w, http.StatusConflict, "This username is taken")
-		} else {
-			HttpResponse(w, http.StatusInternalServerError, "Error creating new user")
-		}
-	} else {
-		json.NewEncoder(w).Encode("Successfully created new user")
-	}
-}
-
-func UserHandler(w http.ResponseWriter, r *http.Request) {
-	//session := sessions.GetSession(r)
-	//authenticated := session.Get("authenticated")
-
-	session,_ := store.Get(r, "session")
-	authenticated := session.Values["authenticated"]
-	vars := mux.Vars(r)
-
-	switch r.Method {
-	case "GET": {
-		if authenticated != nil {
-			fmt.Fprintf(w, "THIS IS USER: %q\n", vars["username"])
-		} else {
-			fmt.Fprint(w, "Not authenticated")
-		}
-		break
-	}
-	case "POST": {
-
-		break
-	}
-	case "DELETE": {
-
-		break
-	}
-	}
-}
-
-func AuthHandler(w http.ResponseWriter, r *http.Request) {
-	//session := sessions.GetSession(r)
-	session,_ := store.Get(r, "session")
-
-	switch r.Method {
-	case "POST": {
-		var user User
-		decoder := json.NewDecoder(r.Body)
-		decoder.Decode(&user)
-
-		//Check if the username exists and correct password was entered
-		var hashedPassword string
-		err := db.QueryRow("SELECT password FROM users WHERE username = ?", user.Username).Scan(&hashedPassword)
-		if err == sql.ErrNoRows || err != nil {
-			HttpResponse(w, http.StatusUnauthorized, "Invalid username")
-			return
-		}
-
-		err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password))
-		if err == bcrypt.ErrMismatchedHashAndPassword {
-			HttpResponse(w, http.StatusUnauthorized, "Invalid password")
-			return
-		}
-
-		//session.Set("authenticated", true)
-		session.Values["username"] = user.Username
-		session.Values["authenticated"] = true
-		session.Save(r, w)
-		HttpResponse(w, http.StatusOK, "Authenticated")
-		break
-	}
-	case "DELETE": {
-		//Clear the current user's session by expiring the cookie
-		session.Options.MaxAge = -1
-		session.Save(r, w)
-		HttpResponse(w, http.StatusOK, "Unauthenticated")
-		break
-	}
-	}
-}
-
-func RequestUtilityHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r, "static/index.html")
-}
-
-func CheckErr(err error) bool {
-	if err != nil {
-		println(err.Error())
-		return true
-	}
-	return false
-}
-
-func HttpResponse(w http.ResponseWriter, statusCode int, message string) {
-	w.WriteHeader(statusCode)
-	w.Write([]byte(message))
-}
+//CREATE TABLE `userdb`.`user_details` (
+//`uuid` CHAR(36) NOT NULL,
+//`key` VARCHAR(200) NOT NULL,
+//`value` VARCHAR(200) NOT NULL,
+//PRIMARY KEY (`uuid`));
